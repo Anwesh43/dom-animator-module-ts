74da9afc005d8a06ba7e2687c53a7d982b0a711e
Anwesh43 <anweshthecool0@gmail.com>
2019-04-02 11:38:57 +0530

    adding code to create blocks

---
+++ BlockCreator.js
@@ -0,0 +1,24 @@
+const w = window.innerWidth
+const h = window.innerHeight
+const n = 10
+const size = Math.min(w, h) / n
+
+const createGreenBlock = (i) => {
+    const div = document.createElement('div')
+    div.style.width = size
+    div.style.height = size
+    div.style.position = 'absolute'
+    div.style.background = 'green'
+    div.style.left = 0
+    div.style.top = i * size
+    document.body.appendChild(div)
+    return div
+}
+
+export const createBlocks = () => {
+    const blocks = []
+    for (var i = 0; i < n; i++) {
+        blocks.push(createGreenBlock(i))
+    }
+    return blocks
+}
