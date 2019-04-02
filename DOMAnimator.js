7ca88f2718cc75b18b2f8522197ef5bd56d81d6a
Anwesh43 <anweshthecool0@gmail.com>
2019-04-02 11:08:51 +0530

    adding State class

---
+++ DOMAnimator.js
@@ -0,0 +1,30 @@
+const scGap = 0.05
+const delay = 30
+
+const updateValue = (scale, dir) => {
+    return scale + dir * scGap
+}
+class State {
+
+    constructor() {
+        this.scale = 0
+        this.dir = 0
+        this.prevScale = 0
+    }
+
+    update(cb) {
+        this.scale = updateValue(this.scale, this.dir)
+        if (Math.abs(this.scale - this.prevScale) > 1) {
+            this.scale = this.prevScale + this.dir
+            this.dir = 0
+            this.prevScale = this.scale
+        }
+    }
+
+    startUpdating(cb) {
+        if (this.dir == 0) {
+            this.dir = 1 - 2 * this.prevScale
+            cb()
+        }
+    }
+}
